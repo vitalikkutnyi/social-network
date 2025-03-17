cb8523646a63e7a259b06e93786d7411c1d4e0d1
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-03-17 04:21:18 +0200

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/Register.css';
+import './styles/Auth.css';
+import './styles/Profile.css';
+import './styles/Post.css';
+import './styles/EditProfile.css';
+import './styles/FollowList.css';
+import './styles/FollowButton.css';
+import './styles/PostDetailPage.css';
+import './styles/Comments.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
