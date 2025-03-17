925672780ca73e4dd7268377ffb09d17f73d7ee0
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-03-17 04:08:57 +0200

    Created FollowingList page

---
+++ frontend/src/pages/FollowingList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FollowList from "../components/FollowList";
+
+const FollowingList = () => {
+    return <FollowList type="following" />
+}
+
+export default FollowingList;
