3c076272283d6f455043f0ce8b1c4b5bc09c99ab
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-03-17 04:12:56 +0200

    Updated

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function Register() {
@@ -8,25 +9,30 @@ function Register() {
     const [password2, setPassword2] = useState('');
     const [error, setError] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate(); 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formData = new FormData();
-        formData.append('username', username);
-        formData.append('password', password);
-        formData.append('password2', password2);
+        const data = {
+            username,
+            password,
+            password2
+        };
 
         try {
-            const response = await axios.post('http://localhost:8000/register/', formData, {
+            const response = await axios.post('http://localhost:8000/register/', data, {
               headers: {
-                'Content-Type': 'multipart/form-data', 
+                'Content-Type': 'application/json', 
               },
             });
       
             if (response.data.message) {
               setMessage(response.data.message);
               setError('');
+              localStorage.setItem('access_token', response.data.access_token);
+              localStorage.setItem('refresh_token', response.data.refresh_token);
+              navigate(`/profile/`);
             } 
         } catch (error) {
             if (error.response && error.response.data.error) {
@@ -38,8 +44,8 @@ function Register() {
         }
     };
     
-        return (
-            <AuthForm 
+    return (
+        <AuthForm 
             title="Реєстрація"
             onSubmit={handleSubmit} 
             username={username}
@@ -50,8 +56,15 @@ function Register() {
             setPassword2={setPassword2}
             error={error}
             message={message}
-            buttonText="Зареєструватися"/>
-          );
-} 
+            buttonText="Зареєструватися"
+            anotherContent={(
+                <>
+                    <p>Вже зареєстровані?</p>
+                    <button type="button" onClick={() => navigate('/login')}>Увійти</button>
+                </>
+            )}
+        />
+    );
+}
 
 export default Register;
