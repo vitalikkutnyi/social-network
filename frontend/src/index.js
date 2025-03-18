3627214c34894bf07319dc90ddb7fe966b917f98
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-03-18 23:09:18 +0200

    Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/App.css';
-import './styles/Sidebar.css';
-import './styles/Auth.css';
-import './styles/Search.css';
-import './styles/Profile.css';
-import './styles/Post.css';
-import './styles/CreatePost.css';
-import './styles/EditProfile.css';
-import './styles/FollowList.css';
-import './styles/FollowButton.css';
-import './styles/PostDetailPage.css';
-import './styles/Comments.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/App.css";
+import "./styles/Sidebar.css";
+import "./styles/Auth.css";
+import "./styles/Search.css";
+import "./styles/Profile.css";
+import "./styles/Post.css";
+import "./styles/CreatePost.css";
+import "./styles/EditProfile.css";
+import "./styles/FollowList.css";
+import "./styles/FollowButton.css";
+import "./styles/PostDetailPage.css";
+import "./styles/Comments.css";
+import "./styles/Chats.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
     <App />
