3be37599e1eb6451ee0046473da3c334490b949c
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:30:43 +0300

    Added LikeList page

---
+++ frontend/src/pages/LikeList.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import API from "../API";
+
+const LikeList = () => {
+  const { username, postId } = useParams();
+  const [likes, setLikes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchLikes = async () => {
+      try {
+        const response = await API.get(
+          `/profile/${username}/posts/${postId}/likes/`
+        );
+        setLikes(response.data);
+      } catch (err) {
+        setError(err.response?.data?.detail || "Не вдалося завантажити лайки.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchLikes();
+  }, [username, postId, navigate]);
+
+  if (loading) return <p>Завантаження...</p>;
+  if (error) return <p className="error">{error}</p>;
+
+  return (
+    <div className="like-list-page">
+      {likes.length === 0 ? (
+        <p>Ще ніхто не лайкнув цей допис.</p>
+      ) : (
+        <>
+          <h2>Кому сподобалось</h2>
+          <ul className="like-list">
+            {likes.map((like) => (
+              <li
+                key={like.user_id}
+                className="like-item"
+                onClick={() => navigate(`/profile/${like.username}/`)}
+              >
+                <div className="user-info">
+                  <img
+                    src={
+                      like.avatar_url
+                        ? like.avatar_url
+                        : "http://127.0.0.1:8000/media/avatars/avatar.jpg"
+                    }
+                    alt="Аватар"
+                    className="like-avatar"
+                  />
+                  <span>{like.username}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LikeList;
