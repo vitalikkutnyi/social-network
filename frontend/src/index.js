86f10cb88f263ec212bb68ee6fdaf2311bfd8313
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:32:10 +0300

    Updated index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/App.css";
+import "./styles/TopSidebar.css";
 import "./styles/Sidebar.css";
+import "./styles/HomePage.css";
 import "./styles/Auth.css";
 import "./styles/Search.css";
 import "./styles/Profile.css";
 import "./styles/Post.css";
 import "./styles/CreatePost.css";
 import "./styles/EditProfile.css";
+import "./styles/LikeList.css";
 import "./styles/FollowList.css";
 import "./styles/FollowButton.css";
 import "./styles/PostDetailPage.css";
 import "./styles/Comments.css";
 import "./styles/Chats.css";
+import "./styles/ChatMessages.css";
+import "./styles/PopularPosts.css";
+import "./styles/Story.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
