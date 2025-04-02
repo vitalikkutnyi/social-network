3df791569ba4efe312e56d2e62e402f69f1b6658
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:45:43 +0300

    Updated urls.py

--- backend/project/src/apps/users/urls.py
+++ backend/project/src/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import UserProfileView, register_view, login_view, logout_view, FollowUserAPIView, FollowersListAPIView, \
-    FollowingListAPIView, UserSearchAPIView, UserProfileEditAPIView, ChatListAPIView, ChatDetailAPIView, \
+from .views import AIChatView, ChatDetailAPIView, UserProfileView, register_view, login_view, logout_view, FollowUserAPIView, FollowersListAPIView, \
+    FollowingListAPIView, UserSearchAPIView, UserProfileEditAPIView, ChatListAPIView, \
     ChatDeleteAPIView, MessageListCreateAPIView, MessageReadAPIView, CreateChatAPIView
 
 urlpatterns = [
@@ -13,11 +13,12 @@ urlpatterns = [
     path('profile/<str:username>/follow/', FollowUserAPIView.as_view(), name="follow_user"),
     path('profile/<str:username>/followers/', FollowersListAPIView.as_view(), name="followers_list"),
     path('profile/<str:username>/following/', FollowingListAPIView.as_view(), name="following_list"),
-    path('search/', UserSearchAPIView.as_view(), name='user_search'),
+    path('users/search/', UserSearchAPIView.as_view(), name='user_search'),
     path('chats/', ChatListAPIView.as_view(), name='chat_list'),
     path('chats/create/', CreateChatAPIView.as_view(), name='create-chat'),
-    path('chats/<int:pk>/', ChatDetailAPIView.as_view(), name='chat_detail'),
+    path('chats/<int:id>/', ChatDetailAPIView.as_view(), name='chat-detail'),
     path('chats/<int:pk>/delete/', ChatDeleteAPIView.as_view(), name='chat_delete'),
     path('chats/<int:chat_id>/messages/', MessageListCreateAPIView.as_view(), name='message-list'),
     path('chats/<int:chat_id>/messages/<int:pk>/read/', MessageReadAPIView.as_view(), name='message-read'),
+    path('ai-chat/', AIChatView.as_view(), name='ai-chat'),
 ]
