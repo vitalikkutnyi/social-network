5d7d4a2b1ee19d9b892df8ebb3703e94bd209f7b
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:45:09 +0300

    Updated admin.py

--- backend/project/src/apps/users/admin.py
+++ backend/project/src/apps/users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
 from .models import CustomUser, Subscription, Chat, Message
+from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
+from django_otp.plugins.otp_totp.models import TOTPDevice
+from django.contrib import admin
+
+class CustomTOTPDeviceAdmin(TOTPDeviceAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.unregister(TOTPDevice)
+admin.site.register(TOTPDevice, CustomTOTPDeviceAdmin)
 
 admin.site.register(CustomUser)
 admin.site.register(Subscription)
