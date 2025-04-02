538b2129b0ca3c21742788c7f74d0fae728ed03a
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:21:30 +0300

    Updated FollowList component

--- frontend/src/components/FollowList.js
+++ frontend/src/components/FollowList.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import FollowButton from "./FollowButton";
+import PageTitle from "../components/PageTitle";
+import API from "../API";
 
 const FollowList = ({ type }) => {
   const { username } = useParams();
@@ -14,16 +15,8 @@ const FollowList = ({ type }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const token = localStorage.getItem("access_token");
-        if (!token) {
-          setError("Увійдіть у систему.");
-          return;
-        }
-
-        const url = `http://127.0.0.1:8000/profile/${username}/${type}/`;
-        const response = await axios.get(url, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
+        const url = `/profile/${username}/${type}/`;
+        const response = await API.get(url);
 
         setUsers(response.data);
       } catch (error) {
@@ -39,51 +32,58 @@ const FollowList = ({ type }) => {
 
   if (loading) return <p>Завантаження...</p>;
   if (error) return <p>{error}</p>;
-  console.log("USERS", users);
-  if (users.length === 0) return <p>Список порожній.</p>;
 
   return (
     <div className="follow-list">
-      <h2>
-        {type === "followers"
-          ? "Підписники користувача "
-          : "Підписки користувача "}
-        <span>{username}</span>
-      </h2>
-      <ul>
-        {users.map((user) => (
-          <li
-            key={user.id}
-            onClick={() =>
-              navigate(
-                user.username === currentUsername
-                  ? "/profile/"
-                  : `/profile/${user.username}/`
-              )
-            }
-          >
-            <div className="user-info">
-              {user.avatar_url ? (
-                <img
-                  src={`http://127.0.0.1:8000${user.avatar_url}`}
-                  alt={user.username}
-                />
-              ) : (
-                <img src={"http://127.0.0.1:8000/media/avatars/avatar.jpg"} />
-              )}
-              <span>{user.username}</span>
-            </div>
-            {user.username !== currentUsername && (
-              <div onClick={(event) => event.stopPropagation()}>
-                <FollowButton
-                  username={user.username}
-                  initialFollowing={user.is_following}
-                />
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+      <PageTitle />
+      {users.length === 0 ? (
+        <p>Список порожній.</p>
+      ) : (
+        <>
+          <h2>
+            {type === "followers"
+              ? "Слідкувачі користувача "
+              : "Слідкування користувача "}
+            <span className="user">{username}</span>
+          </h2>
+          <ul>
+            {users.map((user) => (
+              <li
+                key={user.id}
+                onClick={() =>
+                  navigate(
+                    user.username === currentUsername
+                      ? "/profile/"
+                      : `/profile/${user.username}/`
+                  )
+                }
+              >
+                <div className="user-info">
+                  {user.avatar_url ? (
+                    <img
+                      src={`http://127.0.0.1:8000${user.avatar_url}`}
+                      alt={user.username}
+                    />
+                  ) : (
+                    <img
+                      src={"http://127.0.0.1:8000/media/avatars/avatar.jpg"}
+                    />
+                  )}
+                  <span>{user.username}</span>
+                </div>
+                {user.username !== currentUsername && (
+                  <div onClick={(event) => event.stopPropagation()}>
+                    <FollowButton
+                      username={user.username}
+                      initialFollowing={user.is_following}
+                    />
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
