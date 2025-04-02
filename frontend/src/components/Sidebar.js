af6fc1fde58d5f4cf9b62bead225c76731068799
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2025-04-02 23:21:58 +0300

    Updated Sidebar component

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SiHomepage } from "react-icons/si";
-import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegUserCircle, FaHotjar } from "react-icons/fa";
 import { ImSearch } from "react-icons/im";
 import { TbMessage2Filled } from "react-icons/tb";
+import { BsRobot } from "react-icons/bs";
+import { TbLogout } from "react-icons/tb";
+import API from "../API";
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
-  const goToHomePage = () => navigate("/homepage/");
-  const goToProfile = () => navigate("/profile/");
-  const goToSearch = () => navigate("/search/");
-  const goToChats = () => navigate("/chats/");
+  const goToHomePage = useCallback(() => navigate("/"), [navigate]);
+  const goToProfile = useCallback(() => navigate("/profile/"), [navigate]);
+  const goToSearch = useCallback(() => navigate("/search/"), [navigate]);
+  const goToChats = useCallback(() => navigate("/chats/"), [navigate]);
+  const goToTop = useCallback(() => navigate("/popular-posts/"), [navigate]);
+  const goToAIChat = useCallback(() => navigate("/ai-chat/"), [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      const response = await API.post("/logout/");
+
+      if (response.status === 200) {
+        navigate("/login/");
+        localStorage.removeItem("username");
+        setMessage(response.data.message);
+        setError("");
+      }
+    } catch (error) {
+      console.error("Помилка при виході:", error);
+      setError("Сталася помилка при виході з акаунта");
+      setMessage("");
+    }
+  };
 
   return (
     <div className="sidebar">
@@ -41,7 +65,27 @@ const Sidebar = () => {
             Чати
           </span>
         </li>
+        <li onClick={goToAIChat}>
+          <span className="nav-link">
+            <BsRobot className="nav-icon" />
+            Спілкування з ШІ
+          </span>
+        </li>
+        <li onClick={goToTop}>
+          <span className="nav-link">
+            <FaHotjar className="nav-icon" />
+            Популярне
+          </span>
+        </li>
       </ul>
+      <div className="sidebar-bottom">
+        <div onClick={handleLogout} className="sidebar-logout">
+          <span className="nav-link">
+            <TbLogout className="nav-icon" />
+            Вихід
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
